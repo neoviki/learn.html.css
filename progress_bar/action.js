baffb8e1dfd85da7243a237486eb347cf5fafebf
Vignesh Natarajan <vikiworks.io@gmail.com>
2020-07-11 00:26:14 +0200

    progress bar with javascript

---
+++ progress_bar/action.js
@@ -0,0 +1,45 @@
+let element = document.getElementById("radial_dial_1");
+
+let root = document.documentElement;
+
+var x = 0,
+  prev_x = 0,
+  init_flag = 0;
+
+var ctr = 0;
+
+function mouse_event_listener() {
+  root.addEventListener("mousemove", (e) => {
+    x = e.clientX;
+    if (init_flag == 0) {
+      prev_x = x;
+      init_flag = 1;
+    }
+
+    if (x > prev_x) {
+      ctr = (ctr + (x - prev_x)) % 100;
+    } else {
+      ctr = ctr - (prev_x - x);
+      if (ctr < 0) {
+        ctr = 100;
+      }
+    }
+    element.style.setProperty("--dial_position", ctr);
+    prev_x = x;
+  });
+}
+
+function Sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+async function animate_dial() {
+  while (1) {
+    ctr = (ctr + 1) % 100;
+    element.style.setProperty("--dial_position", ctr);
+    await Sleep(100);
+  }
+}
+
+//mouse_event_listener();
+animate_dial();
